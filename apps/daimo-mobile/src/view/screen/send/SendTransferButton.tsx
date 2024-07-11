9a819fb07cd95a3cf16aeb02a758cf572f279070
Andrew <andrewliu08@gmail.com>
2024-07-11 12:24:18 -0400

    make RPC call to landline for deposits

--- apps/daimo-mobile/src/view/screen/send/SendTransferButton.tsx
+++ apps/daimo-mobile/src/view/screen/send/SendTransferButton.tsx
@@ -8,6 +8,7 @@ import {
   dollarsToAmount,
   hasAccountName,
 } from "@daimo/common";
+import { daimoChainFromId } from "@daimo/contract";
 import {
   DaimoNonce,
   DaimoNonceMetadata,
@@ -22,10 +23,12 @@ import {
   useSendWithDeviceKeyAsync,
 } from "../../../action/useSendAsync";
 import { useExitToHome } from "../../../common/nav";
+import { BankTransferOptions } from "../../../logic/bankTransferOptions";
 import {
-  BridgeBankAccountContact,
+  LandlineBankAccountContact,
   EAccountContact,
 } from "../../../logic/daimoContacts";
+import { getRpcFunc } from "../../../logic/trpc";
 import { Account } from "../../../storage/account";
 import { getAmountText } from "../../shared/Amount";
 import { LongPressBigButton } from "../../shared/Button";
@@ -40,14 +43,16 @@ export function SendTransferButton({
   memo,
   minTransferAmount = 0,
   route,
+  landlineBankTransferOption,
 }: {
   account: Account;
-  recipient: EAccountContact | BridgeBankAccountContact;
+  recipient: EAccountContact | LandlineBankAccountContact;
   dollars: number;
   toCoin: ForeignToken;
   memo?: string;
   minTransferAmount?: number;
   route?: ProposedSwap | null;
+  landlineBankTransferOption?: BankTransferOptions | null;
 }) {
   console.log(`[SEND] rendering SendButton ${dollars}`);
 
@@ -119,6 +124,26 @@ export function SendTransferButton({
   })();
   const disabled = sendDisabledReason != null || dollars === 0;
 
+  let handlePress = undefined;
+  if (landlineBankTransferOption === BankTransferOptions.Deposit) {
+    assert(
+      recipient.type === "landlineBankAccount",
+      "Recipient of Landline deposit must be a landline bank account"
+    );
+    // TODO: authenticate this call
+    handlePress = () => {
+      const rpcFunc = getRpcFunc(daimoChainFromId(account.homeChainId));
+      rpcFunc.depositFromLandline.mutate({
+        daimoAddress: account.address,
+        landlineAccountId: recipient.landlineAccountId,
+        amount: dollarsStr,
+        memo,
+      });
+    };
+  } else {
+    handlePress = exec;
+  }
+
   const button = (function () {
     switch (status) {
       case "idle":
@@ -126,7 +151,7 @@ export function SendTransferButton({
         return (
           <LongPressBigButton
             title="HOLD TO SEND"
-            onPress={disabled ? undefined : exec}
+            onPress={disabled ? undefined : handlePress}
             type="primary"
             disabled={disabled}
             duration={400}
